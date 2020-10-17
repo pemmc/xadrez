076efaf69dd5d30747ee18601c0f59b47cf5ce21
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-17 18:49:49 -0300

    Cor das peças

--- JododeXadrez/Program.cs
+++ JododeXadrez/Program.cs
@@ -13,22 +13,19 @@ namespace JododeXadrez
             try
             {
 
-                /*
+               
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
- 
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
+
+                tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(3, 5));
+
                 Tela.imprimirTabuleiro(tab);
-                */
+             
 
-                PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-
-                Console.WriteLine(pos);
-
-                Console.WriteLine(pos.toPosicao());
-
+               
                 Console.ReadLine();
             }
 
--- JododeXadrez/Tela.cs
+++ JododeXadrez/Tela.cs
@@ -14,6 +14,8 @@ namespace JododeXadrez
             for (int i = 0; i < tab.linhas; i++)
             {
 
+                Console.Write(8 - i + " ");
+
                 for (int j = 0; j < tab.colunas; j++)
                 {
 
@@ -23,17 +25,42 @@ namespace JododeXadrez
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        Tela.imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
 
-                    }
-
-                    
-
+                    } 
                 }
 
                 Console.WriteLine();
 
             }
+
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+
+                Console.Write(peca);
+
+            }
+
+            else if(peca.cor == Cor.Amarela)
+            {
+
+                ConsoleColor aux = Console.ForegroundColor;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.Write(peca);
+
+                Console.ForegroundColor = aux;
+
+            }
+
         }
     }
+
 }
