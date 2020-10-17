5d316f32c405b535e235a556949d7b151d585b08
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-17 18:25:46 -0300

    Tratando erros com excecao personalizada

--- JododeXadrez/Program.cs
+++ JododeXadrez/Program.cs
@@ -10,17 +10,26 @@ namespace JododeXadrez
         static void Main(string[] args)
         {
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,4));
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+ 
+                Tela.imprimirTabuleiro(tab);
 
-            Tela.imprimirTabuleiro(tab);
+                Console.ReadLine();
+            }
 
-            Console.ReadLine();
+            catch (TabuleiroException e)
+            {
 
+                Console.WriteLine("Error Tabuleiro: " + e.Message);
+
+            }
         }
     }
 }
---
+++ JododeXadrez/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JododeXadrez.tabuleiro
+{
+    public class TabuleiroException : Exception
+     {
+        public TabuleiroException(string msg) : base (msg)
+        {
+
+        }
+    }
+}
